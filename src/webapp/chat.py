e9d8aee79e3e041079497380663f776ff6615bcc
Alexander Voloshin <sannjka.v@gmail.com>
2024-11-04 17:41:28 +0300

    Add private chats

--- src/webapp/chat.py
+++ src/webapp/chat.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 from fastapi import WebSocket, WebSocketDisconnect, APIRouter
 
@@ -7,39 +7,61 @@ chat_router = APIRouter()
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[Dict[str, WebSocket]] = []
+        self.active_connections: List[Dict[Tuple(str), WebSocket]] = []
 
-    async def connect(self, client_id: str, websocket: WebSocket):
+    async def connect(
+            self, client_id: str, friend_id: str, websocket: WebSocket,
+        ):
         await websocket.accept()
-        self.active_connections.append((client_id, websocket))
+        self.active_connections.append(((client_id, friend_id), websocket))
 
-    def disconnect(self, client_id: str, websocket: WebSocket):
-        self.active_connections.remove((client_id, websocket))
+    def disconnect(
+            self, client_id: str, friend_id: str, websocket: WebSocket,
+        ):
+        self.active_connections.remove(((client_id, friend_id), websocket))
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: str, client_id):
-        for id, connection in self.active_connections:
-            if id != client_id:
-                await connection.send_text(message)
+        for (cl_id, fr_id), connection in self.active_connections:
+            if cl_id != client_id and fr_id == 'common':
+                await self.send_personal_message(message, connection)
 
+    async def send_private_message(self, message: str, client_id, friend_id):
+        for (cl_id, fr_id), connection in self.active_connections:
+            if cl_id == friend_id and fr_id == client_id:
+                await self.send_personal_message(message, connection)
 
 manager = ConnectionManager()
 
 
-@chat_router.websocket('/ws/{client_id}')
-async def websocket_endpoint(websocket: WebSocket, client_id: str):
-    await manager.connect(client_id, websocket)
+@chat_router.websocket('/ws/{client_id}/{friend_id}')
+async def websocket_endpoint(
+        websocket: WebSocket,
+        client_id: str,
+        friend_id: str,
+    ):
+    await manager.connect(client_id, friend_id, websocket)
     try:
         while True:
             data = await websocket.receive_text()
             personal_message = f'You say: {data}'
             common_message = f'{client_id} says: {data}'
             await manager.send_personal_message(personal_message, websocket)
-            await manager.broadcast(common_message, client_id)
+            if friend_id == 'common':
+                await manager.broadcast(common_message, client_id)
+            else:
+                await manager.send_private_message(
+                        common_message, client_id, friend_id,
+                )
     except WebSocketDisconnect:
-        manager.disconnect(client_id, websocket)
+        manager.disconnect(client_id, friend_id, websocket)
         left_message = f'{client_id} left the chat'
-        await manager.broadcast(left_message, client_id)
+        if friend_id == 'common':
+            await manager.broadcast(left_message, client_id)
+        else:
+            await manager.send_private_message(
+                    left_message, client_id, friend_id,
+            )
 
--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -1,10 +1,10 @@
 import pytest
 
 
-def test_ws_send_get_message(ws_client):
+def test_ws_send_get_message_private_chat(ws_client):
     with (
-            ws_client.websocket_connect('/ws/1') as websocket1,
-            ws_client.websocket_connect('/ws/2') as websocket2
+            ws_client.websocket_connect('/ws/1/2') as websocket1,
+            ws_client.websocket_connect('/ws/2/1') as websocket2
         ):
         websocket1.send_text('hey')
         data1 = websocket1.receive_text()
@@ -12,9 +12,27 @@ def test_ws_send_get_message(ws_client):
     assert data1 == 'You say: hey'
     assert data2 == '1 says: hey'
 
-def test_ws_left_chat(ws_client):
-    with ws_client.websocket_connect('/ws/1') as websocket1:
-        with ws_client.websocket_connect('/ws/2') as websocket2:
+def test_ws_send_get_message_common_chat(ws_client):
+    with (
+            ws_client.websocket_connect('/ws/1/common') as websocket1,
+            ws_client.websocket_connect('/ws/2/common') as websocket2
+        ):
+        websocket1.send_text('hey')
+        data1 = websocket1.receive_text()
+        data2 = websocket2.receive_text()
+    assert data1 == 'You say: hey'
+    assert data2 == '1 says: hey'
+
+def test_ws_left_chat_private_chat(ws_client):
+    with ws_client.websocket_connect('/ws/1/2') as websocket1:
+        with ws_client.websocket_connect('/ws/2/1') as websocket2:
+            pass
+        data1 = websocket1.receive_text()
+    assert data1 == '2 left the chat'
+
+def test_ws_left_chat_common_chat(ws_client):
+    with ws_client.websocket_connect('/ws/1/common') as websocket1:
+        with ws_client.websocket_connect('/ws/2/common') as websocket2:
             pass
         data1 = websocket1.receive_text()
     assert data1 == '2 left the chat'
